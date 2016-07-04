a31d614dac1e51606c92d8b371c2f3a692907391
tangsq <tangsq@expert-db.com>
2016-07-04 21:40:40 +0800

    modify os check error

--- install.sh
+++ install.sh
@@ -76,7 +76,7 @@ EOF
 
 path=$(cd `dirname $0`;pwd )
 ( ( [ -n "$(grep CentOS /etc/issue)" ] \
-  || [ -n "$(grep CentOS /etc/centos-release)" ] )
+  || [ -n "$(grep CentOS /etc/centos-release)" ] ) \
   && ( yum install gcc g++ make vim pam-devel tcp_wrappers-devel unzip httpd-tools -y ) ) \
   || ( [ -n "$(grep -E 'Debian|Ubuntu' /etc/issue)" ] \
   && ( apt-get update ) \
